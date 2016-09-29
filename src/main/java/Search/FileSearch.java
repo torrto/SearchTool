70b77aa02afc2442e9a029e5467318f403d0461d
Daniel Melton <danielmelton@techtonics.io>
2016-09-29 00:59:24 -0400

    Update FileSearch.java

--- src/main/java/Search/FileSearch.java
+++ src/main/java/Search/FileSearch.java
@@ -1,6 +1,8 @@
 package Search;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by rhy704 on 9/22/16.
@@ -10,6 +12,7 @@ public class FileSearch {
     private static File file;
     private static String name;
     private static boolean found = false;
+    private static List<String > messages = new ArrayList<String>();
 
     private FileSearch(){}
 
@@ -21,20 +24,24 @@ public class FileSearch {
 
     public static void setFile(File file) { FileSearch.file = file; }
 
+    public static List<String> getMessages() {
+        return messages;
+    }
+
+    public static void setMessages(List<String> messages) {
+        FileSearch.messages = messages;
+    }
+
     public static void searcher(File file) {
             for (File each : file.listFiles()) {
                 if (each.isDirectory()) {
-                System.out.println(each.getName() + " is a directory");
+//                System.out.println(each.getName() + " is a directory");
                     searcher(each);
                 } else {
-                System.out.println(each.getName() + " is a file");
-                    if (FileSearch.name.equalsIgnoreCase(each.getName())) {
-                        System.out.println("FOUND YA: " + each.getAbsolutePath());
-                        found = true;
+//                System.out.println(each.getName() + " is a file");
+                    if (each.getName().matches("(.*)" + FileSearch.name + "(.*)")) {
+                        messages.add(each.getAbsolutePath());
                     }
-                }
-                if(found == true) {
-                    break;
                 }
             }
         }
